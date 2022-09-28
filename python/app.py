f9d167499b9d06596e6f6cbaa99a796f88b4cc32
milan <milan@milan.milan>
2022-09-28 18:55:31 +0200

    initial

---
+++ python/app.py
@@ -0,0 +1,116 @@
+from blockchain import *
+import pandas as PD
+from hexbytes import (
+    HexBytes,
+)
+
+
+if not isConnectedToETH():
+    raise Exception(f"NOT CONNECTED TO BLOCKCHAIN NETWORK")
+
+# Find more emojis here: https://www.webfx.com/tools/emoji-cheat-sheet/
+ST.set_page_config(page_title="ETH CRAWLER", page_icon=":computer:", layout="wide")
+
+ST.header("ETH CRAWLER")
+notificationBox: ST.container = ST.container()
+with ST.form("wallet_form", True):
+    walletInput: str       = ST.text_input("Wallet Address:", value="", key="wallet", placeholder="0x5fXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+    blockInput:  Number    = ST.number_input("Starting block")
+    dateInput:   DateValue = ST.date_input("Filter by date(2015-07-29 will be skipped)", key="date", min_value=Date(2015, 7, 29), max_value=Date.today(), value=Date(2015, 7, 29))
+    formSubmit:  bool      = ST.form_submit_button("Check")
+    data:        Any       = None
+    if formSubmit:
+        print(blockInput)
+        notificationBox.empty()
+
+        if walletInput is None or not isWalletAddressValid(walletInput.strip()):
+            notificationBox.error(f"Invalid wallet provided {walletInput}!")
+            ST.stop()
+
+        if dateInput == Date(2015, 7, 29):
+            # ETH released 2015-07-30
+            # skip date filtering
+            dateInput = None
+
+        with ST.spinner(f"Loading for {walletInput}..."):
+            data = getFilteredTransactions(walletInput.strip(), blockInput, dateInput)
+        
+        if len(data) == 0:
+            notificationBox.warning("No data found for given wallet!")
+            ST.stop()
+        
+        #contentBox = ST.container()
+        with ST.container():
+            for block in data:
+                with ST.expander(f"Block: {block.number}\n {block.hash.hex()}"):
+                    tabOverview, tabTxs = ST.tabs(["Overview", "Transactions"])
+                    with tabOverview:
+                        ST.caption(f"Block number: {block.number}")
+                        ST.caption(f"Block size: {block.size}")
+                        ST.caption(f"Block hash: {block.hash.hex()}")
+                        ST.caption(f"Mix hash: {block.mixHash.hex()}")
+                        if "miner" in block:
+                            ST.caption(f"Miner: {block.miner}")
+                        if "parentHash" in block:
+                            ST.caption(f"Parent hash: {block.parentHash.hex()}")
+                        ST.caption(f"Gas limit: {convertUnit(block.gasLimit)} ETH")
+                        ST.caption(f"Gas used: {convertUnit(block.gasUsed)} ETH")
+                        ST.caption(f"UTC Time: {Datetime.utcfromtimestamp(block.timestamp)}")
+                        if "nonce" in block:
+                            ST.caption(f"Nonce: {block.nonce.hex()}")
+                        if "receiptRoot" in block:
+                            ST.caption(f"Receipt Root: {block.receiptRoot.hex()}")
+                        if "stateRoot" in block:
+                            ST.caption(f"Stete root: {block.stateRoot.hex()}")
+                        if "difficulty" in block:
+                            ST.caption(f"Difficulty: {block.difficulty}")
+                        if "totalDifficulty" in block:
+                            ST.caption(f"Total Difficulty: {block.totalDifficulty}")
+                        if "sha3Uncles" in block:
+                            ST.caption(f"Sha3Uncles: {block.sha3Uncles.hex()}")
+                        if "uncles" in block:
+                            ST.caption(f"Uncles: {block.uncles}")
+                        if "logsBloom" in block:
+                            ST.caption(f"Logs Bloom: {block.logsBloom.hex()}")
+                        if "extraData" in block:
+                            ST.caption(f"Extra Data: {block.extraData.hex()}")
+                        if "transactionsRoot" in block:
+                            ST.caption(f"Transactions Root: {block.transactionsRoot.hex()}")
+                    
+                    with tabTxs:
+                        if len(block.transactions) == 0:
+                            ST.header("No transactions found.")
+                            ST.stop()
+                        
+                        columns = ["accessList", "blockHash", "blockNumber", "chainId", "data", "from", "gas", "gasPrice", "maxFeePerGas", "maxPriorityFeePerGas", "hash", "input", "nonce", "r", "s", "to", "transactionIndex", "type", "v", "value"]
+                        txsData = []
+
+                        for tx in block.transactions:
+                            temp = [
+                                str(tx.get("accessList", [])),
+                                str(tx.get("blockHash", HexBytes("")).hex()),
+                                str(tx.get("blockNumber", 0)),
+                                str(tx.get("chainId", "/")),
+                                str(tx.get("data", HexBytes("")).hex()),
+                                str(tx.get("from", "/")),
+                                str(tx.get("gas", 0)),
+                                str(tx.get("gasPrice", 0)),
+                                str(tx.get("maxFeePerGas", 0)),
+                                str(tx.get("maxPriorityFeePerGas", 0)),
+                                str(tx.get("hash", HexBytes("")).hex()),
+                                str(tx.get("input", "/")),
+                                str(tx.get("nonce", 0)),
+                                str(tx.get("r", HexBytes("")).hex()),
+                                str(tx.get("s", HexBytes("")).hex()),
+                                str(tx.get("to", "/")),
+                                str(tx.get("transactionIndex", 0)),
+                                str(tx.get("type", 0)),
+                                str(tx.get("v", 0)),
+                                str(tx.get("value", 0))
+                            ]
+                            txsData.append(temp)
+                                                
+                        ST.dataframe(PD.DataFrame(txsData,columns=columns))
+
+
+
---
+++ python/blockchain.py
@@ -0,0 +1,159 @@
+from web3 import Web3
+from web3.types import BlockData
+import requests as Requests
+import time as Time
+import sys as Sys
+from datetime import datetime as Datetime, date as Date
+import os as OS
+from typing_extensions import TypeAlias
+from typing import Union, Any
+import streamlit as ST
+
+
+ETHSCAN:   str       = "3SIH628PJECRU2FQHDG8AFS9J17J8K7NMJ"
+INFURA:    str       = "79a3a2554bd04a81b5e4a8bcd53d675f"
+DateValue: TypeAlias = Union[Date, Datetime, None]
+Number:    TypeAlias = Union[int, float]
+
+
+
+def getWeb3() -> Web3:
+    """Initialize Web3 object."""
+    # Making connection with infura url for collecting ETH transactions
+    infuraUrl: str = f"https://mainnet.infura.io/v3/{INFURA}"
+    return Web3(Web3.HTTPProvider(infuraUrl))
+
+
+def isConnectedToETH() -> bool:
+    """Check if connection to ETH blockchain is established."""
+    w3: Web3 = getWeb3()
+    return w3.isConnected()
+
+
+def isWalletAddressValid(ethAddress: str) -> bool:
+    """Check if provided addresss is valid."""
+    w3: Web3 = getWeb3()
+    return w3.isAddress(ethAddress)
+
+
+def getLatestBlockNumber() -> int:
+    """Get latest processed block on ETH blockchain."""
+    w3: Web3 = getWeb3()
+    return w3.eth.block_number
+
+
+def convertUnit(amount: int, unit: str="ether") -> float:
+    """Convert unit."""
+    units: str  = "wei/kwei/babbage/femtoether/mwei/lovelace/picoether/gwei/shannon/nanoether/nano/szabo/microether/micro/finney/milliether/milli/ether/kether/grand/mether/gether/tether"
+    if unit not in units:
+        #raise Exception(f"INVALID UNIT PROVIDED: {currencyUnit}")
+        print(f"INVALID UNIT PROVIDED: {unit}")
+        return -1
+    return float(Web3.fromWei(amount, unit))
+
+
+def getBalance(ethAddress: str, currencyUnit: str="ether") -> float:
+    """Get balance based on given address."""
+    units: str  = "wei/kwei/babbage/femtoether/mwei/lovelace/picoether/gwei/shannon/nanoether/nano/szabo/microether/micro/finney/milliether/milli/ether/kether/grand/mether/gether/tether"
+    w3:    Web3 = getWeb3()
+    if not w3.isAddress(ethAddress):
+        #raise Exception(f"INVALID ETH ADDRESS PROVIDED: {ethAddress}")
+        print(f"INVALID ETH ADDRESS PROVIDED: {ethAddress}")
+        return -1
+    
+    if currencyUnit not in units:
+        #raise Exception(f"INVALID UNIT PROVIDED: {currencyUnit}")
+        print(f"INVALID UNIT PROVIDED: {currencyUnit}")
+        return -1
+
+    if not w3.isChecksumAddress(ethAddress):
+        ethAddress = w3.toChecksumAddress(ethAddress)
+
+    balance = w3.eth.get_balance(ethAddress)
+
+    return float(w3.fromWei(balance, currencyUnit))
+
+
+def getAllTransactions(ethAddress: str) -> list:
+    """Get all transactions for given ETH address."""
+    
+    w3: Web3 = getWeb3()
+    if not w3.isAddress(ethAddress):
+        #raise Exception(f"INVALID ETH ADDRESS PROVIDED: {ethAddress}")
+        print(f"INVALID ETH ADDRESS PROVIDED: {ethAddress}")
+        return []
+
+    ethScanUrl: str = f"https://api.etherscan.io/api?module=account&action=txlist&startblock=0&offset=10000&sort=asc&apikey={ETHSCAN}&address={ethAddress}"
+
+    try:
+        Time.sleep(1) # 5 limit per second may be reached
+        response: Requests.Response = Requests.get(ethScanUrl)
+        if response.status_code != 200:
+            raise Exception(f"INVALID HTTP RESPONSE({response.status_code})")
+        
+        data: Any = response.json()
+
+        if type(data) is not dict:
+            raise Exception(f"[1]API ERROR - INVALID DATA TYPE: {type(data)}")
+
+        if data.get("message", "~OK") != "OK" and data.get("status", -1) != 1:
+            raise Exception(f"[2]API ERROR: {data}")
+        
+        return data.get("result")
+
+    except Exception as e:
+        print("HTTP ERROR: ", e)
+        return []
+
+@ST.cache()
+def getFilteredTransactions(ethAddress: str, startBlock: int=0, filteredDate: DateValue=None) -> list:
+    """Get all transactions based on given filters."""
+    try:
+        w3:           Web3 = getWeb3()
+        transactions: list = []
+        
+        if not w3.isAddress(ethAddress):
+            #raise Exception(f"INVALID ETH ADDRESS PROVIDED: {ethAddress}")
+            print(f"INVALID ETH ADDRESS PROVIDED: {ethAddress}")
+            return transactions
+        
+        ethScanUrl: str = f"https://api.etherscan.io/api?module=account&action=txlist&startblock=0&offset=10000&sort=asc&apikey={ETHSCAN}&address={ethAddress}"
+
+        Time.sleep(1) # 5 limit per second may be reached
+        response = Requests.get(ethScanUrl)
+        if response.status_code != 200:
+            raise Exception(f"INVALID HTTP RESPONSE({response.status_code})")
+        
+        returnData = response.json()
+        
+        if returnData.get("message", "~OK") != "OK" and returnData.get("status", -1) != 1:
+            raise Exception(f"[1]API ERROR: {returnData}")
+        
+        for block in returnData.get("result", []):
+            if type(block) is not dict:
+                raise Exception(f"[2]API ERROR: INVALID DATA RESULTS: {block}")
+            
+            blockNum: int = int(block.get("blockNumber"))
+
+            if startBlock > blockNum:
+                # only blocks that are higher then start block are allowed
+                continue
+            
+            loopBlock: BlockData = w3.eth.get_block(blockNum, full_transactions=True)
+            blockTime: DateValue = Datetime.utcfromtimestamp(loopBlock.timestamp)
+
+            if filteredDate is not None:
+                if blockTime.date() != filteredDate:
+                    # block not created on same day
+                    continue
+
+            transactions.append(loopBlock)
+
+        return transactions
+
+    except:
+        _, _, excTb = Sys.exc_info()
+        print(f"FAILED TO GET TRANSACTIONS DUE TO :: {str(Sys.exc_info())} ::: file: " + OS.path.abspath(excTb.tb_frame.f_code.co_filename) + " line:" + str(excTb.tb_lineno))
+        return []
+
+
